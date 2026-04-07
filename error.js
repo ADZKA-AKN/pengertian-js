3ebcb3c4fd234cdd13d153dd93d60cb03d65904f
ADZKA-AKN <muhammadadzkarullah@gmail.com>
2026-04-07 13:11:59 +0700

    error

--- error.js
+++ error.js
@@ -1,16 +1,92 @@
 //throw error
 
-const age = 9
+// const age = 9
 
-if (age <= 0) {
-    throw new Error('umur tidak valid')
-}
+// if (age <= 0) {
+//     throw new Error('umur tidak valid')
+// }
 
 //error hnadling
+//Soal 1
+function cekUmur(umur) {
+  //kita coba eksekusi, jika ada error dia akan ke block catch
+  try {
+    //cek apakah umurnya angka
+    if (isNaN(umur)) {
+      throw new Error("Umur harus angka");
+    }
 
-try {
-    const hasil = 10/0
-    console.log(hasil)
-} catch (error) {
- console.error(error.message)   
+    //cek apakah umurnya < 0
+    if (umur < 0) {
+      throw new Error("Umur tidak Valid");
+    }
+
+    console.log("Umur:", umur);
+  } catch (error) {
+    //tangkap errornya
+    console.log(error.message);
+  } finally {
+    console.log("Selesai");
+  }
 }
+
+cekUmur(-5);
+
+//Soal 2
+function parseData(parsingdong) {
+  try {
+    let hasil = JSON.parse(parsingdong);
+    console.log(hasil);
+  } catch (error) {
+    console.log(error.message);
+    console.log("JSON nggak valid");
+  }
+}
+
+parseData('{nama": "Irwan"}');
+
+//Soal 3
+function transfer(saldoRekening, jumlahTransfer) {
+  try {
+    //validasi jika jumlah yang ditransfer lebih besar daripada saldo rekening
+    if (saldoRekening < jumlahTransfer) {
+      throw new Error("Saldo Anda tidak Mencukupi");
+    } else {
+      console.log("Transfer Berhasil");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+transfer(100000, 200000);
+
+//Soal 4
+function bagiAngka(a, b) {
+  try {
+    if (b === 0) {
+      throw new Error("Tidak bisa dibagi dengan 0");
+    }
+    let hasil = a / b;
+    console.log(`Hasil Pembagian: ${hasil}`);
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+bagiAngka(5, 5);
+
+//Soal 5
+const validasiUmur = (umur) => {
+    try {
+        if (umur < 18){
+            throw new Error ("Umur belum mencukupi")
+        }
+        console.log("Akses diberikan")
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+validasiUmur(14);
+transfer(1000, 500)
